7b3070ea769757041bfd4005766329a9bea58506
canterbery <wotphantommail@gmail.com>
2023-10-17 20:34:43 +0300

    zb-2: + form , styles

--- frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,16 +1,55 @@
-import { Button } from '~/libs/components/components.js';
+import { Button, Input, Link } from '~/libs/components/components.js';
+import styles from './styles.module.scss';
+import { useAppForm } from '~/libs/hooks/hooks.js';
+import { DEFAULT_LOGIN_PAYLOAD } from './libs/constants/constants.js';
+import { userSignInValidationSchema } from '~/packages/users/users.js';
+import { AppRoute } from '~/libs/enums/enums.js';
 
 type Properties = {
   onSubmit: () => void;
 };
 
-const SignInForm: React.FC<Properties> = () => (
-  <>
-    <h1>Sign In</h1>
-    <form>
-      <Button label="Sign in" />
-    </form>
-  </>
-);
+const SignInForm: React.FC<Properties> = () => {
+  const { control, errors } = useAppForm({
+    defaultValues: DEFAULT_LOGIN_PAYLOAD,
+    validationSchema: userSignInValidationSchema,
+  });
+  return (
+    <div className={styles.formWrapper}>
+      <h2 className={styles.authFormTitle}>Sign In</h2>
+      <form className={styles.form} name="SignInForm">
+        <fieldset className={styles.fieldset}>
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            label="Email"
+            control={control}
+            errors={errors}
+          />
+          <Input
+            name="password"
+            label="Password"
+            errors={errors}
+            control={control}
+            placeholder="Enter password"
+          />
+        </fieldset>
+        <p className={styles.forgotPasswordText}>
+          <Link className={styles.forgotPasswordLink} to={AppRoute.ROOT}>
+            Forgot password?
+          </Link>
+        </p>
+        <Button label="Sign In" hasFullWidth classname={styles.signInBtn} />
+        <p className={styles.signUpText}>
+          <span>Dont have account? </span>
+          <Link to={AppRoute.SIGN_UP} className={styles.signUpLink}>
+            Sign Up
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export { SignInForm };
