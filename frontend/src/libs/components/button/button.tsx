a7a6347cc1f8e20dd9c9ee40e213610b4b3d1789
canterbery <wotphantommail@gmail.com>
2023-10-17 15:37:09 +0300

    zb-1: + header

--- frontend/src/libs/components/button/button.tsx
+++ frontend/src/libs/components/button/button.tsx
@@ -1,10 +1,20 @@
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
+import styles from './styles.module.scss';
+
 type Properties = {
   label: string;
   type?: 'button' | 'submit';
+  classname?: string;
 };
 
-const Button: React.FC<Properties> = ({ type = 'button', label }) => (
-  <button type={type}>{label}</button>
+const Button: React.FC<Properties> = ({
+  type = 'button',
+  label,
+  classname = '',
+}) => (
+  <button className={getValidClassNames(styles.button, classname)} type={type}>
+    {label}
+  </button>
 );
 
 export { Button };
