ec726a351bfebb5f2c112bd1372abdc76da22f95
canterbery <wotphantommail@gmail.com>
2023-10-17 20:33:23 +0300

    zb-2: + prop , styles

--- frontend/src/libs/components/link/link.tsx
+++ frontend/src/libs/components/link/link.tsx
@@ -2,14 +2,19 @@ import { NavLink } from 'react-router-dom';
 
 import { type AppRoute } from '~/libs/enums/enums.js';
 import { type ValueOf } from '~/libs/types/types.js';
+import styles from './styles.module.scss';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
 
 type Properties = {
   to: ValueOf<typeof AppRoute>;
   children: React.ReactNode;
+  className?: string;
 };
 
-const Link: React.FC<Properties> = ({ children, to }) => (
-  <NavLink to={to}>{children}</NavLink>
+const Link: React.FC<Properties> = ({ children, to, className = '' }) => (
+  <NavLink className={getValidClassNames(styles.link, className)} to={to}>
+    {children}
+  </NavLink>
 );
 
 export { Link };
