d5e91c4b3d3c950ef7b923785529bf530cb1d7a4
canterbery <wotphantommail@gmail.com>
2023-10-19 18:32:03 +0300

    zb-8: + responsive styles

--- frontend/src/pages/landing/landing.tsx
+++ frontend/src/pages/landing/landing.tsx
@@ -1,7 +1,14 @@
 import { Button } from '~/libs/components/components.js';
 import styles from './styles.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { useCallback } from '~/libs/hooks/hooks.js';
+import { AppRoute } from '~/libs/enums/app-route.enum.js';
 
 const Landing: React.FC = () => {
+  const navigate = useNavigate();
+  const hadleGetStartedClick = useCallback(() => {
+    navigate(AppRoute.SIGN_IN);
+  }, [navigate]);
   return (
     <div className={styles.landingWrapper}>
       <p className={styles.title}>The chemical negatively charged</p>
@@ -16,6 +23,7 @@ const Landing: React.FC = () => {
         type="button"
         label="Get Started"
         classname={styles.getStartedBtn}
+        onClick={hadleGetStartedClick}
       />
     </div>
   );
