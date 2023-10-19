0b3ccdeaef0cb3da9873fa8a6c9c14fe11efbeda
canterbery <wotphantommail@gmail.com>
2023-10-19 12:52:02 +0300

    zb-7: + encrypt

--- backend/src/packages/auth/auth.ts
+++ backend/src/packages/auth/auth.ts
@@ -3,8 +3,9 @@ import { userService } from '~/packages/users/users.js';
 
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
+import { encrypt } from '~/libs/packages/encrypt/encrypt.js';
 
-const authService = new AuthService(userService);
+const authService = new AuthService(userService, encrypt);
 const authController = new AuthController(logger, authService);
 
 export { authController, authService };
