5066f77935ad16c632f798b8924ebb244f94ae45
canterbery <wotphantommail@gmail.com>
2023-10-19 16:38:28 +0300

    zb-8: + sign out button

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -1,21 +1,32 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Button } from '../components.js';
 import styles from './styles.module.scss';
 import { useCallback } from 'react';
 import { AppRoute } from '~/libs/enums/app-route.enum.js';
+import { type UserAuthResponseDto } from '~/packages/users/users.js';
+import { useAppDispatch } from '~/libs/hooks/hooks.js';
+import { actions as authActions } from '~/slices/auth/auth.js';
 
-const Header: React.FC = () => {
+type Properties = {
+  user: UserAuthResponseDto | null;
+};
+
+const Header: React.FC<Properties> = ({ user }) => {
   const navigate = useNavigate();
-  const { pathname } = useLocation();
+  const dispatch = useAppDispatch();
+
   const handleLogInClick = useCallback(() => {
     navigate(AppRoute.SIGN_IN);
   }, [navigate]);
   const handleSignUpClick = useCallback(() => {
     navigate(AppRoute.SIGN_UP);
   }, [navigate]);
+  const handleSignOutClick = useCallback(() => {
+    void dispatch(authActions.logout());
+  }, [dispatch]);
   return (
     <div className={styles.header}>
-      {pathname === AppRoute.ROOT && (
+      {!user && (
         <>
           <Button
             label="Sign In"
@@ -28,6 +39,13 @@ const Header: React.FC = () => {
             onClick={handleSignUpClick}
           />
         </>
+      )}
+      {user && (
+        <Button
+          label="Sign Out"
+          classname={styles.signUpBtn}
+          onClick={handleSignOutClick}
+        />
       )}
     </div>
   );
