ed546f078307d4b5c20fda6a8b589ff178b0005b
canterbery <wotphantommail@gmail.com>
2024-05-15 19:12:18 +0300

    evp-4: + types

--- backend/src/packages/participants/libs/types/types.ts
+++ backend/src/packages/participants/libs/types/types.ts
@@ -1,1 +1,3 @@
 export { type ParticipantEntityInstance } from 'shared/build/index.js';
+export { type ParticipantRegistrationRequestDto } from 'shared/build/index.js';
+export { type ParticipantRegistrationResponseDto } from 'shared/build/index.js';
