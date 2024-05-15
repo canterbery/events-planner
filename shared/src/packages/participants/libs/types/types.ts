195733c470da044f31513144061ec7723ef7fd6c
canterbery <wotphantommail@gmail.com>
2024-05-15 19:16:58 +0300

    evp-4: + types , validation

--- shared/src/packages/participants/libs/types/types.ts
+++ shared/src/packages/participants/libs/types/types.ts
@@ -1,1 +1,3 @@
 export { type ParticipantEntityInstance } from './participant-entity-instance.type.js';
+export { type ParticipantRegistrationRequestDto } from './participant-registration-request-dto.type.js';
+export { type ParticipantRegistrationResponseDto } from './participant-registration-response-dto.type.js';
--- shared/src/packages/participants/participants.ts
+++ shared/src/packages/participants/participants.ts
@@ -1,2 +1,7 @@
-export { type ParticipantEntityInstance } from './libs/types/types.js';
+export {
+  type ParticipantEntityInstance,
+  type ParticipantRegistrationRequestDto,
+  type ParticipantRegistrationResponseDto,
+} from './libs/types/types.js';
 export { ParticipantsApiPath, ParticipantsSource } from './libs/enums/enums.js';
+export { participantRegistration as registrationValidationSchema } from './libs/validation-schemas/validation-schemas.js';
