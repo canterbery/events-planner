5c0e45fea044f2299dea0baef57f9bcca722d791
canterbery <wotphantommail@gmail.com>
2024-05-16 12:12:42 +0300

    evp-6: + find all param

--- backend/src/libs/interfaces/repository.interface.ts
+++ backend/src/libs/interfaces/repository.interface.ts
@@ -1,6 +1,6 @@
 interface IRepository<T = unknown> {
   find(id: number): Promise<T | null>;
-  findAll(): Promise<T[]>;
+  findAll(sortOption: string | null): Promise<T[]>;
   create(payload: unknown): Promise<T>;
   update(): Promise<T>;
   delete(): Promise<boolean>;
