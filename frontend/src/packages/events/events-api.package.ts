54ea5fe0d9e34cb4cf952945b4a217ed99dbf665
canterbery <wotphantommail@gmail.com>
2024-05-16 08:03:09 +0300

    evp-5: + action + api

--- frontend/src/packages/events/events-api.package.ts
+++ frontend/src/packages/events/events-api.package.ts
@@ -5,6 +5,7 @@ import { type IStorage } from '~/libs/packages/storage/storage.js';
 
 import { EventsApiPath } from './libs/enums/enums.js';
 import { type EventEntityInstance } from './libs/types/types.js';
+import { type ParticipantRegistrationResponseDto } from '../participants/participants.js';
 
 type Constructor = {
   baseUrl: string;
@@ -28,6 +29,23 @@ class EventsApi extends HttpApi {
     );
 
     return await response.json<{ items: EventEntityInstance[] }>();
+  }
+
+  public async getParticipantsByEventId(
+    eventId: number,
+  ): Promise<ParticipantRegistrationResponseDto[] | null> {
+    const response = await this.load(
+      this.getFullEndpoint(EventsApiPath.$ID_PARTICIPANTS, {
+        id: eventId.toString(),
+      }),
+      {
+        method: 'GET',
+        contentType: ContentType.JSON,
+        hasAuth: false,
+      },
+    );
+
+    return await response.json<ParticipantRegistrationResponseDto[] | null>();
   }
 }
 
